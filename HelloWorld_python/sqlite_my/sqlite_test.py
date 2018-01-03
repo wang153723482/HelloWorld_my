c340a4ef8d1aa14fb257acdbb15434df2aa84987
wangchao <wang153723482@gmail.com>
2018-01-03 15:36:50 +0800

    补充update

--- HelloWorld_python/sqlite_my/sqlite_test.py
+++ HelloWorld_python/sqlite_my/sqlite_test.py
@@ -8,16 +8,20 @@ cu = cx.cursor()
 
 # cu.execute("create table catalog (id integer primary key,pid integer,name varchar(10) UNIQUE,nickname text '')")
 
-# 
+
 # for t in[(0,10,'abc','Yu'),(1,20,'cba','Xu')]:
 #     cx.execute("insert into catalog values (?,?,?,?)", t)
-#     
 # cx.commit()
 
-
-sql = 'create table ziru_house(room_id,room_name,price,mianji,chaoxiang,huxing,louceng,jiaotong,url,quyu,shangquan,create_date)'
-cx.execute(sql)
+for t in [(100,0),(200,1)]:
+    cx.execute('update catalog set pid=? where id =?',t)
 cx.commit()
 
-# cu.execute("select * from catalog") 
-# print cu.fetchall()
+
+
+# sql = 'create table ziru_house(room_id,room_name,price,mianji,chaoxiang,huxing,louceng,jiaotong,url,quyu,shangquan,create_date)'
+# cx.execute(sql)
+# cx.commit()
+
+cu.execute("select * from catalog") 
+print cu.fetchall()
